9713a193941b4ec1aaca625561f06dc9eb68c6b7
Dylan Anthony <dbanty@users.noreply.github.com>
2023-10-28 12:14:48 -0600

    Fix style violations

--- Formula/knope.rb
+++ Formula/knope.rb
@@ -1,5 +1,5 @@
 class Knope < Formula
-  desc "Automate tedious developer tasks, like generating changelogs and updating versions"
+  desc "Automate developer tasks, like generating changelogs and updating versions"
   homepage "https://knope-dev.github.io/knope/"
   url "https://github.com/knope-dev/knope/archive/refs/tags/v0.13.0.tar.gz"
   version "0.13.0"
@@ -10,20 +10,10 @@ class Knope < Formula
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test knope`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/knope", "--help"
   end
 end
