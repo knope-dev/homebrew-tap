11a0474378c0dcf118236778f59f4ac141b67b4b
Dylan Anthony <dbanty@users.noreply.github.com>
2023-10-28 12:17:20 -0600

    Remove explicit version

--- Formula/knope.rb
+++ Formula/knope.rb
@@ -2,7 +2,6 @@ class Knope < Formula
   desc "Automate developer tasks, like generating changelogs and updating versions"
   homepage "https://knope-dev.github.io/knope/"
   url "https://github.com/knope-dev/knope/archive/refs/tags/v0.13.0.tar.gz"
-  version "0.13.0"
   sha256 "2f5136c38a3386e80325e712069c21984f6fa3aff830b9c40ab9fdaec630f7b2"
   license "MIT"
   head "https://github.com/apollographql/rover.git", branch: "main"
