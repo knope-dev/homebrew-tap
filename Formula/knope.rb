9ab681234ff7d2974889526d64e62ed909a5830b
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2023-11-17 13:27:07 -0700

    Release 0.13.2

--- Formula/knope.rb
+++ Formula/knope.rb
@@ -1,13 +1,13 @@
 class Knope < Formula
   desc "Automate developer tasks, like generating changelogs and updating versions"
   homepage "https://knope-dev.github.io/knope/"
-  url "https://github.com/knope-dev/knope/archive/refs/tags/v0.13.0.tar.gz"
+  url "https://github.com/knope-dev/knope/archive/refs/tags/v0.13.2.tar.gz"
   sha256 "2f5136c38a3386e80325e712069c21984f6fa3aff830b9c40ab9fdaec630f7b2"
   license "MIT"
   head "https://github.com/apollographql/rover.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/knope-dev/homebrew-tap/releases/download/knope-0.13.0"
+    root_url "https://github.com/knope-dev/homebrew-tap/releases/download/knope-0.13.2"
     rebuild 2
     sha256 cellar: :any,                 ventura:      "ebcd75c4724244248fe13a63b65eefc5e9577e0b380ec4f5a998c6816555734a"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "26c38dead9bec195b0ac4b02b807141fcfa50525011ca2459d5001914ff150c5"
