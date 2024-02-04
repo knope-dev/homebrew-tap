4df88517ea27f21121b0f6a76d03b212527b828e
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2024-02-04 02:30:25 +0000

    knope: update 0.13.4 bottle.

--- Formula/knope.rb
+++ Formula/knope.rb
@@ -7,10 +7,10 @@ class Knope < Formula
   head "https://github.com/apollographql/rover.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/knope-dev/homebrew-tap/releases/download/knope-0.13.0"
-    rebuild 2
-    sha256 cellar: :any,                 ventura:      "ebcd75c4724244248fe13a63b65eefc5e9577e0b380ec4f5a998c6816555734a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "26c38dead9bec195b0ac4b02b807141fcfa50525011ca2459d5001914ff150c5"
+    root_url "https://github.com/knope-dev/homebrew-tap/releases/download/knope-0.13.4"
+    sha256 cellar: :any,                 arm64_sonoma: "1b45289ae37d5074baf9c998083ddb999faacd7a6b606eaa36ed46a35330d38a"
+    sha256 cellar: :any,                 ventura:      "e3dbee6ab9034ceb6eb89915959aa972427e31e1930e83cc7d351ac7d0af597b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0b25370e69afbd98245d6948c522ac69585f1154a93982d12c808507b729d455"
   end
 
   depends_on "rust" => :build
