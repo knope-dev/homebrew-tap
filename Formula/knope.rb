3480861b2241cb866292e5745c9fb9beb3eb703b
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2025-01-21 05:12:43 +0000

    knope: update 0.18.3 bottle.

--- Formula/knope.rb
+++ Formula/knope.rb
@@ -7,10 +7,10 @@ class Knope < Formula
   head "https://github.com/knope-dev/knope.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/knope-dev/homebrew-tap/releases/download/knope-0.18.2"
-    sha256 cellar: :any,                 arm64_sonoma: "a710eea704a733ececd900818c77efecf9842118cc0a72f984c79103a060a800"
-    sha256 cellar: :any,                 ventura:      "19c22a6119cfc1341981f28f2b3787db8b17e2792a1d6666c621258c09649e16"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7f89def869a320d05e851e85854a79fc96a2feb655259eb451a5b249455c9373"
+    root_url "https://github.com/knope-dev/homebrew-tap/releases/download/knope-0.18.3"
+    sha256 cellar: :any,                 arm64_sonoma: "6d383239ea2d0391ffa5df91ab8db5a4aee0a73192b0b86c023133180b49d10c"
+    sha256 cellar: :any,                 ventura:      "5efe747ba456af04ce25df900a918fd09cad759bc2e604b4cd1398df6ade5fac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "39a0b3baaa4eaa4adc2b1aeb316987dbe911d43a341f441120c5ed44fa55069c"
   end
 
   depends_on "rust" => :build
